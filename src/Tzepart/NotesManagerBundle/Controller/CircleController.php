5266d7e67dff14aa98a1b1aeafc76a239beff4a1
TzepART <tzepart@gmail.com>
2016-05-09 23:34:33 +0400

    Create CRUD for Circle

---
+++ src/Tzepart/NotesManagerBundle/Controller/CircleController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Tzepart\NotesManagerBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Tzepart\NotesManagerBundle\Entity\Circle;
+use Tzepart\NotesManagerBundle\Form\CircleType;
+
+/**
+ * Circle controller.
+ *
+ */
+class CircleController extends Controller
+{
+    /**
+     * Lists all Circle entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $circles = $em->getRepository('NotesManagerBundle:Circle')->findAll();
+
+        return $this->render('circle/index.html.twig', array(
+            'circles' => $circles,
+        ));
+    }
+
+    /**
+     * Creates a new Circle entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $circle = new Circle();
+        $form = $this->createForm('Tzepart\NotesManagerBundle\Form\CircleType', $circle);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($circle);
+            $em->flush();
+
+            return $this->redirectToRoute('circle_show', array('id' => $circle->getId()));
+        }
+
+        return $this->render('circle/new.html.twig', array(
+            'circle' => $circle,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Circle entity.
+     *
+     */
+    public function showAction(Circle $circle)
+    {
+        $deleteForm = $this->createDeleteForm($circle);
+
+        return $this->render('circle/show.html.twig', array(
+            'circle' => $circle,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Circle entity.
+     *
+     */
+    public function editAction(Request $request, Circle $circle)
+    {
+        $deleteForm = $this->createDeleteForm($circle);
+        $editForm = $this->createForm('Tzepart\NotesManagerBundle\Form\CircleType', $circle);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($circle);
+            $em->flush();
+
+            return $this->redirectToRoute('circle_edit', array('id' => $circle->getId()));
+        }
+
+        return $this->render('circle/edit.html.twig', array(
+            'circle' => $circle,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Circle entity.
+     *
+     */
+    public function deleteAction(Request $request, Circle $circle)
+    {
+        $form = $this->createDeleteForm($circle);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($circle);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('circle_index');
+    }
+
+    /**
+     * Creates a form to delete a Circle entity.
+     *
+     * @param Circle $circle The Circle entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Circle $circle)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('circle_delete', array('id' => $circle->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
