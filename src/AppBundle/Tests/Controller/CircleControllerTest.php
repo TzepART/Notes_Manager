3d8728d9d73f60d052bdccdd95b0bad7d463ffe6
tzepart <tzepart@gmail.com>
2016-11-30 01:23:45 +0300

    w_53_fix_entities_for_tests

--- src/AppBundle/Tests/Controller/CircleControllerTest.php
+++ src/AppBundle/Tests/Controller/CircleControllerTest.php
@@ -2,12 +2,21 @@
 
 namespace AppBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 class CircleControllerTest extends WebTestCase
 {
     public function testCompleteScenario()
     {
+        $this->loadFixtures(array(
+            'AppBundle\DataFixtures\ORM\LoadCircleData',
+            'AppBundle\DataFixtures\ORM\LoadLabelsData',
+            'AppBundle\DataFixtures\ORM\LoadLayersData',
+            'AppBundle\DataFixtures\ORM\LoadNotesData',
+            'AppBundle\DataFixtures\ORM\LoadSectorsData',
+            'AppBundle\DataFixtures\ORM\LoadUserData',
+        ));
 
+        $client = static::createClient();
     }
 }
